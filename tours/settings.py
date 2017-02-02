9eb8dfd211abc3ceb3f4dba84da8510a2109cdcf
Richard Sun <richardsun29@ucla.edu>
2017-02-01 17:57:15 -0800

    set up basic view

--- tours/settings.py
+++ tours/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '138.68.20.219', 'tours.brui
 # Application definition
 
 INSTALLED_APPS = [
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
