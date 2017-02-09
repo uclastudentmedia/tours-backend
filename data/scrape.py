8e4ec566472c6768c57f6241c10e4eb1a9499fd9
Michael Li <michael.li11702@gmail.com>
2017-02-09 15:15:30 -0800

    Loop through location ids and scrape

--- data/scrape.py
+++ data/scrape.py
@@ -3,24 +3,46 @@ import sys
 from bs4 import BeautifulSoup
 
 def get_page(location_id):
+    # returns xml data from the endpoint for a location id
     endpoint = "http://space.admin.ucla.edu/locations_plsql/pkg_ucla.getlocation"
-    url = endpoint + "?locid=" + location_id
-    print "url:", url
+    url = endpoint + "?locid=" + str(location_id)
+    #print "url:", url
     ret_xml = urllib2.urlopen(url).read()
     soup = BeautifulSoup(ret_xml, "lxml")
     return soup.location # all data is under a <location>
 
-def main():
-    if (len(sys.argv) < 2):
-        print "Usage: python scrape.py [LOCATION_ID]"
-        return
-    xml_data = get_page(sys.argv[1])
+def get_data_for(location_id):
+    # returns object for location id, None if error
+    xml_data = get_page(location_id)
+    if xml_data is None:
+        print "No landmark data for ", location_id
+        return None
     landmark_name = xml_data["map_name"]
-    coordinates = xml_data.coordinates.find_all("coordinate")[1]
+    coordinates = xml_data.coordinates.find_all("coordinate")
+    if len(coordinates) == 0:
+        print "No coordinate data for ", location_id
+        return None
+    coordinates = coordinates[1]
     note = xml_data.note.string
-    print "name is", landmark_name
-    print "lat =", coordinates['y'], "long =", coordinates['x']
-    print "Note is: ", note 
+    if note is None:
+        note = "No description right now for this location."
+    #print "name is", landmark_name
+    #print "lat =", coordinates['y'], "long =", coordinates['x']
+    #print "Note is: ", note 
+    ret_obj = {
+            "id": location_id,
+            "name": landmark_name,
+            "lat": coordinates['y'],
+            "long": coordinates['x'],
+            "note": note
+    }
+    print ret_obj
+    return ret_obj
+
+def main():
+    for id in range(1, 413):
+        get_data_for(id)
+    return
 
 if __name__ == "__main__":
     main()
