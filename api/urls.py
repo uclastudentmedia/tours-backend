40106a24679449f3ef1ed0dbc4baf43fe3a1f948
Richard Sun <richardsun29@ucla.edu>
2017-02-16 20:32:22 -0800

    add category to landmark list, /api/category endpoint

--- api/urls.py
+++ api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^landmark/$', views.landmark_list, name='landmark'),
     url(r'^landmark/(?P<id>[0-9]+)/$', views.landmark_detail),
+    url(r'^category/$', views.category_list, name='category'),
 ]
--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse
 from django.forms.models import model_to_dict
 from django.http import Http404
 
-from .models import Landmark
+from .models import Landmark, Category
 
 def index(request):
     context = {}
@@ -14,7 +14,8 @@ def landmark_list(request):
     landmarks = Landmark.objects.values('id',
                                         'name',
                                         'lat',
-                                        'long')
+                                        'long',
+                                        'category')
     
     
     return JsonResponse({
@@ -28,3 +29,11 @@ def landmark_detail(request, id):
     except Landmark.DoesNotExist:
         raise Http404("Landmark does not exist")
     return JsonResponse({ "results": model_to_dict(landmark) })
+
+def category_list(request):
+    categories = Category.objects.values('id',
+                                        'name',
+                                        'sort_order')
+    return JsonResponse({
+        "results": list(categories)
+    })
