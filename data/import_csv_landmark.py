62bd4ad926b2012c3cb41cc31fe7e2cad7851f6a
Richard Sun <richardsun29@ucla.edu>
2017-03-16 13:53:22 -0700

    csv import script

---
+++ data/import_csv_landmark.py
@@ -0,0 +1,23 @@
+import csv
+
+from api.models import Landmark, Category
+
+def import_landmarks(csv_filepath):
+    with open(csv_filepath, 'r', encoding='utf-8') as fd:
+        #id,name,lat,long,text_description,category_id,category,norm_id
+        dr = csv.DictReader(fd)
+
+        landmarks = []
+        for r in dr:
+            if not r['category_id']:
+                category = None
+            else:
+                category = Category.objects.get(id=r['category_id'])
+            landmark = Landmark(id=r['norm_id'],
+                                name=r['name'],
+                                lat=r['lat'],
+                                long=r['long'],
+                                text_description=r['text_description'],
+                                category=category)
+            landmarks.append(landmark)
+        Landmark.objects.bulk_create(landmarks)
