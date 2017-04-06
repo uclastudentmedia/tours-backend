0f953052ebbd6334e3cacb04ec8877d20bd48c2f
Richard Sun <richardsun29@ucla.edu>
2017-04-06 20:53:05 +0000

    allow serving static files from media/

--- tours/urls.py
+++ tours/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -21,3 +23,5 @@ urlpatterns = [
     url(r'^api/', include('api.urls')),
     url(r'^photologue/', include('photologue.urls', namespace='photologue')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
