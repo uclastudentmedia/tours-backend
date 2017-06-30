19ae80c4251b2b56e123fd87596a3f036649c997
Michael Li <michael.li11702@gmail.com>
2017-06-30 06:01:09 +0000

    Return floor plan image in API

--- indoor/urls.py
+++ indoor/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name='indoor'
 urlpatterns=[
-	url(r'^indoor-nav/(?P<landmark_id>[0-9]+)/(?P<start>[0-9]+)/(?P<end>[0-9]+)$', views.navigation_route),
+	#url(r'^indoor-nav/(?P<landmark_id>[0-9]+)/(?P<start>[0-9]+)/(?P<end>[0-9]+)$', views.navigation_route),
+	url(r'^indoor-nav/(?P<building_name>[0-9]+)/(?P<floor>[A-Z0-9]+)/$', views.navigation_route),
 ]
--- indoor/views.py
+++ indoor/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
-from api.models import Landmark
+#from api.models import Landmark
 from PIL import Image
 import magic
 import random
@@ -11,12 +11,23 @@ def generate_image(landmark_id, start, end):
     image = Image.new("RGB", (800, 600), random.choice(INK))
     return image
 
-def navigation_route(request, landmark_id, start, end):
+def get_floor_plan_image(building_name, floor_string):
+    try:
+        filename = "../assets/img/floor_plans/" + building_name + "_" + floor_string + ".png"
+        img = Image.open(filename)
+    except:
+        raise Http404("Image " + filename + " does not exist or can't be opened")
+    return img
+
+def navigation_route(request, building_name, floor):
+    """
     try:
         landmark = Landmark.objects.get(id=int(landmark_id))
     except:
         raise Http404("Landmark does not exist")
     image=generate_image(landmark_id, start, end)
+    """
+    image = get_floor_plan_image(building_name, floor)
     response=HttpResponse(image,content_type="image/png")
     image.save(response, "PNG")
     return response
