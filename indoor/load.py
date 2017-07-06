5b9c1a2ea66b53d24da18b77dda5848a933dbb36
Michael Li <michael.li11702@gmail.com>
2017-07-06 06:44:21 +0000

    Add model and loading for paths

--- indoor/load.py
+++ indoor/load.py
@@ -3,6 +3,7 @@ from .models import *
 
 rooms_shp = "/var/www/html/indoors-gis/ackerman/2/ackerman-2-rooms.shp"
 points_shp = "/var/www/html/indoors-gis/ackerman/2/ackerman-2-points.shp"
+paths_shp = "/var/www/html/indoors-gis/ackerman/2/ackerman-2-paths.shp"
 
 def run(verbose=True):
     rooms_lm = LayerMapping(RoomPolygon, rooms_shp, roompolygon_mapping,
@@ -11,3 +12,8 @@ def run(verbose=True):
     points_lm = LayerMapping(Point, points_shp, point_mapping,
             transform=False,encoding='iso-8859-1')
     points_lm.save(strict=True, verbose=verbose)
+
+def load_paths(verbose=True):
+    paths_lm = LayerMapping(Path, paths_shp, path_mapping,
+            transform=False,encoding='iso-8859-1')
+    paths_lm.save(strict=True, verbose=verbose)
--- indoor/models.py
+++ indoor/models.py
@@ -22,3 +22,13 @@ point_mapping = {
     'type' : 'type',
     'geom' : 'POINT',
 }
+
+class Path(models.Model):
+    path_id = models.IntegerField()
+    geom = models.LineStringField(srid=4236)
+
+# Auto-generated `LayerMapping` dictionary for Path model
+path_mapping = {
+    'path_id' : 'id',
+    'geom' : 'LINESTRING',
+}
