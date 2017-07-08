dab6e3b713945b815b822a0bf752117531b0a488
JayKun <junkaiong11@gmail.com>
2017-07-08 05:08:05 +0000

    added return route and room function

--- indoor/urls.py
+++ indoor/urls.py
@@ -5,4 +5,5 @@ app_name='indoor'
 urlpatterns=[
 	#url(r'^indoor-nav/(?P<landmark_id>[0-9]+)/(?P<start>[0-9]+)/(?P<end>[0-9]+)$', views.navigation_route),
 	url(r'^(?P<building_name>\w+)/(?P<floor>\w+)$', views.navigation_route),
+        url(r'^(?P<landmark_id>[0-9]+)/(?P<start_room>\w+)/(?P<end_room>\w+)/(?P<start_end>[0-1])', views.navigation_image),
 ]
--- indoor/views.py
+++ indoor/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
-#from api.models import Landmark
+from api.models import Landmark
 from PIL import Image
 import magic
 import random
@@ -9,7 +9,7 @@ import os.path
 
 INK = "red", "blue", "green", "yellow"
 
-def generate_image(landmark_id, start, end):
+def generate_image():
     image = Image.new("RGB", (800, 600), random.choice(INK))
     return image
 
@@ -23,14 +23,25 @@ def get_floor_plan_image(building_name, floor_string):
     return img
 
 def navigation_route(request, building_name, floor):
-    """
-    try:
-        landmark = Landmark.objects.get(id=int(landmark_id))
-    except:
-        raise Http404("Landmark does not exist")
-    image=generate_image(landmark_id, start, end)
-    """
     image = get_floor_plan_image(building_name, floor)
     response=HttpResponse(content_type="image/png")
     image.save(response, "PNG")
     return response
+
+def navigation_image(request, landmark_id, start_room, end_room, start_end):
+    '''  
+    try:
+        landmark = Landmark.objects.get(id=int(landmark_id))
+    except:
+        raise Http404("Landmark does not exist")
+    '''
+    #start is 0
+    if (start_end==0):
+        image=generate_image(); #returns starting room highlighted and path drawn
+    #end is 1
+    else:
+        image=generate_image(); #returns image with ending room highlighted
+    response=HttpResponse(content_type="image/png")
+    image.save(response, "PNG")
+    return response
+
