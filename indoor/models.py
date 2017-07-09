d35224c26ee3fde96e355638625333b6461c6d37
Richard Sun <richardsun29@ucla.edu>
2017-07-09 04:24:47 +0000

    define __str__ for indoor models

--- indoor/models.py
+++ indoor/models.py
@@ -5,27 +5,38 @@ class RoomPolygon(models.Model):
     name = models.CharField(max_length=80)
     geom = models.PolygonField(srid=4326)
 
-# Auto-generated `LayerMapping` dictionary for indoor model
+    def __str__(self):
+        return self.name
+
+# Auto-generated `LayerMapping` dictionary for RoomPolygon model
 roompolygon_mapping = {
     'name' : 'name',
     'geom' : 'POLYGON',
 }
+
 
 class POI(models.Model):
     name = models.CharField(max_length=80)
     type = models.CharField(max_length=80)
     geom = models.PointField(srid=4326)
 
-# Auto-generated `LayerMapping` dictionary for RoomPolygon model
+    def __str__(self):
+        return self.name
+
+# Auto-generated `LayerMapping` dictionary for POI model
 poi_mapping = {
     'name' : 'name',
     'type' : 'type',
     'geom' : 'POINT',
 }
 
+
 class Path(models.Model):
     path_id = models.IntegerField()
     geom = models.LineStringField(srid=4236)
+
+    def __str__(self):
+        return str(self.path_id)
 
 # Auto-generated `LayerMapping` dictionary for Path model
 path_mapping = {
