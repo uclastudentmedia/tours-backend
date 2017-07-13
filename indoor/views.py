55c614300c64f1b8e65698c39316d4dbaa8c1cec
Michael Li <michael.li11702@gmail.com>
2017-07-13 06:24:10 +0000

    Draw route from room to room, use images that work with Pillow

--- indoor/views.py
+++ indoor/views.py
@@ -2,7 +2,9 @@ from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from api.models import Landmark
-from PIL import Image
+from .navigation import route
+from PIL import Image, ImageDraw
+import matplotlib.pyplot as plt
 import magic
 import random
 import os.path
@@ -14,7 +16,7 @@ def generate_image():
     return image
 
 def get_floor_plan_image(building_name, floor_string):
-    filename = "floor_plans/" + building_name + "_" + floor_string + ".png"
+    filename = "floor_plans/" + building_name.title() + "_" + floor_string + ".png"
     newfilename = os.path.join(settings.MEDIA_ROOT, filename)
     try:
         img = Image.open(newfilename)
@@ -22,8 +24,22 @@ def get_floor_plan_image(building_name, floor_string):
         raise Http404("Image " + newfilename + " does not exist or can't be opened")
     return img
 
-def navigation_route(request, building_name, floor):
+def navigation_route(request, building_name, floor, start_room, end_room):
+    # TODO: probably clean up etc.
     image = get_floor_plan_image(building_name, floor)
+    graph = route(building_name, start_room, end_room)[0]
+    draw = ImageDraw.Draw(image)
+    line_fill = (52, 152, 219, 255)
+    for e in graph.edges():
+        edgeList = [(e[0][0], -e[0][1]), (e[1][0], -e[1][1])]
+        #print edgeList
+        draw.line(edgeList, fill=line_fill, width=20)
+    for n in graph.nodes():
+        #print n
+        x = n[0]
+        y = -n[1]
+        rad = 15
+        draw.ellipse([x - rad, y - rad, x + rad, y + rad], fill=line_fill)
     response=HttpResponse(content_type="image/png")
     image.save(response, "PNG")
     return response
