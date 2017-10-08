e47eadcdb436f2593d566b2c3954db4b70aeab61
JayKun <junkaiong11@gmail.com>
2017-10-08 07:42:01 +0000

    added mapzen scrapper

---
+++ data/get_mapzen_data.py
@@ -0,0 +1,37 @@
+import urllib, json
+from api.models import Tour
+
+tours = Tour.objects.all()
+for tour in tours:
+    landmarks = tour.landmarks.all()
+    query = {}
+    query["locations"] = list()
+    for landmark in landmarks:
+        coord={}
+        coord["lat"] = landmark.lat
+        coord["lon"] = landmark.long
+        query["locations"].append(coord)
+
+    query["costing"] = "pedestrian"
+    query["directions_options"] = {}
+    query["directions_options"]["units"] = "miles"
+    
+    #construct url
+    api_key ="mapzen-kRYtVFg"
+    url = "https://matrix.mapzen.com/optimized_route?json="+json.dumps(query) + "&api_key=" + "mapzen-kRYtVFg"
+    print url
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+
+    #time and duration
+    tour.distance = data['trip']['summary']['length']
+    tour.duration = data['trip']['summary']['time']
+
+    #polyline
+    data =  data["trip"]["locations"]
+    print data
+    for location in data:
+        location.pop('original_index')
+        location.pop('type')
+        tour.polyline = data
+    tour.save()
